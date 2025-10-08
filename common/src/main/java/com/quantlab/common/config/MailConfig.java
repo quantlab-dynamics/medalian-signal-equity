c78d1614adab6f31a7888f01894922619da7f423
bhargavagonugunta <bhargavagonugunta123@gmail.com>
2025-10-08 10:39:15 +0530

    remove of the email sending

--- common/src/main/java/com/quantlab/common/config/MailConfig.java
+++ common/src/main/java/com/quantlab/common/config/MailConfig.java
@@ -14,8 +14,8 @@ public class MailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("engineering@quantlabalgo.com");
-        mailSender.setPassword("qkpa vffn ryli chvk");
+        mailSender.setUsername("engine78ering@quantlyuabalgo.com");
+        mailSender.setPassword("qkpa vffnop ryli opchvk");
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
